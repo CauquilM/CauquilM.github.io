23c08cdff997caa48879226577cc6237d30881c2
MathCoder <mathieufc31@gmail.com>
2022-08-02 15:46:58 +0200

    remove testing idb-keyval code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { entries, get, set } from "idb-keyval";
+import "console-success";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
